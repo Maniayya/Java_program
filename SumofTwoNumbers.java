e6d42c4b92bbbf823aa6019f8ec9377bef2f7ef6
Maniayya E <114644140+Maniayya@users.noreply.github.com>
2024-10-02 11:26:21 +0530

    Add files via upload

---
+++ SumofTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SumofTwoNumbers {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the value A");
+        int a=obj.nextInt();
+        System.out.println("Enter the value B");
+        int b=obj.nextInt();
+        System.out.println("Sum of Two Num: " + (a+b));
+        
+    }
+}
